fb5c48d8c92ac0fc3a08e8747f4c90e9dc50822b
rayan.haidar <rayan.haidar188@hotmail.com>
2026-01-20 20:34:59 +0200

    chore: add base package structure and rename main application class

--- src/main/java/com/rayanhaidar/usermanagement/UsermanagementApiApplication.java
+++ src/main/java/com/rayanhaidar/usermanagement/UsermanagementApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UsermanagementApiApplication {
+public class UserManagementApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UsermanagementApiApplication.class, args);
+		SpringApplication.run(UserManagementApiApplication.class, args);
 	}
 
 }
